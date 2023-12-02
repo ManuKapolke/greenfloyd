12e9d3a611ff68d3aab217fad29f4c04a602ee0c
Manu Kapolke <manukapolke@gmail.com>
2023-12-02 22:10:18 +0100

    landing page and font

---
+++ src/app/components/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent {
+  images = [
+    'greenfloyd_elbershallen (1).JPG',
+    'greenfloyd_elbershallen (2).JPG',
+    'greenfloyd_elbershallen (3).JPG',
+    'greenfloyd_elbershallen (4).JPG',
+    'greenfloyd_elbershallen (5).JPG',
+    'greenfloyd_elbershallen (6).JPG',
+    'greenfloyd_elbershallen (7).JPG',
+    'greenfloyd_elbershallen (8).JPG',
+    'greenfloyd_elbershallen (9).JPG',
+    'greenfloyd_elbershallen (10).JPG'
+  ];
+  headlines = [
+    'Pink Floyd im großen Klanggewand',
+    'So klingt Zukunftsmusik',
+    'Ganz viele Gänsehautmomente',
+    'Ein grandioses Finale',
+    'Die große Überwältigung',
+    'Pink Floyd im großen Klanggewand',
+    'So klingt Zukunftsmusik',
+    'Ganz viele Gänsehautmomente',
+    'Ein grandioses Finale',
+    'Die große Überwältigung'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
